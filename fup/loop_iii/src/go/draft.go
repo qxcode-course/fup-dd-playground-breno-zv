a7c58ac44a7eea384ec16d017e51b8c892c62d40
breno renan <brenorenan.dev@gmail.com>
2026-05-26 17:57:28 +0000

    mikaele

--- fup/loop_iii/src/go/draft.go
+++ fup/loop_iii/src/go/draft.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var a, b int64
+	var a, b int
 
 	fmt.Scan(&a, &b)
-	fmt.Printf("[ ")
-	    for i := a; i < b; i--{
-		    fmt.Printf("%d ", i)
-	}
-	fmt.Printf("]\n")
+	fmt.Printf("[")
+
+	    for i := a; i > b; i-- {
+		    fmt.Printf(" %d", i)
+		}
+
+	fmt.Printf(" ]\n")
 
 }
--- fup/tv/src/go/draft.go
+++ fup/tv/src/go/draft.go
@@ -1,31 +1,14 @@
 package main
 import "fmt"
 func main() {
-    var value, parc float64
+    var value, parc, porcJu float64
 
-    fmt.Scan("%f", &value)
-    fmt.Scan("%f", &parc)
+    fmt.Scanf("%f", &value)
+    fmt.Scanf("%f", &parc)
 
-    if parc == 1{
-        fmt.Print(parc)
-    }else if parc ==2{
-        fmt.Print(parc+(0.05*parc))
-    }else if parc == 3{
-        fmt.Print(parc+(0.05*parc))
-    }else if parc == 4{
-        
-    }else if parc == 5{
-        
-    }else if parc == 6{
-        
-    }else if parc == 7{
-        
-    }else if parc == 8{
-        
-    }else if parc == 9{
-        
-    }else{
-        
-    }
+    porcJu = ((parc-1)*5)/100
+
+    fmt.Printf("%.2f\n", ((value*porcJu)+value)/parc)
+    fmt.Printf("%.2f\n", value*porcJu+value)
 
 }
